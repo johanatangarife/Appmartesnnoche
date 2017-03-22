727d25da94035659f90bdc64715df284c6d29d39
johana tangarife <johana tangarife>
2017-03-21 20:14:25 -0500

    se creo una vista que imprime en un label

--- AppMartes/app/src/main/java/com/example/usuario/appmartes/MainActivity.java
+++ AppMartes/app/src/main/java/com/example/usuario/appmartes/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.usuario.appmartes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        label1= (TextView) findViewById(R.id.label1);
+        boton= (Button) findViewById(R.id.button);
+        boton.setOnclickListener(This);
+    }
+
+    @Override
+    public void OnClick(view v){
+        switch (v.get.id()){
+            case R.id.button;
+                label1.setText("se presiona el boton");
+                Toast.makeText(getApplicationContext), "se oprime el boton", Toast.LENGTH_LONG)
+
+        }
     }
 }
